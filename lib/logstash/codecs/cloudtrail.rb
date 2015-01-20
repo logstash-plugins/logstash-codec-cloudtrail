a4fd7ad68aa6719f6b6e77c707f22c664d6ecd4f
Pier-Hugues Pellerin <phpellerin@gmail.com>
2015-01-20 15:37:08 -0500

    Removing milestone option

--- lib/logstash/codecs/cloudtrail.rb
+++ lib/logstash/codecs/cloudtrail.rb
@@ -6,7 +6,6 @@ require "logstash/json"
 # This is the base class for logstash codecs.
 class LogStash::Codecs::CloudTrail < LogStash::Codecs::Spool
   config_name "cloudtrail"
-  milestone 1
 
   public
   def decode(data)
