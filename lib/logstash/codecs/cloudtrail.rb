a745cb19e6f6a2d45fa07e335ca76d7f2bdcc69b
Vrinceanu Catalin <catalin.vrinceanu@stratalux.com>
2016-06-10 21:07:54 +0300

    Fixed mapping error from elasticsearch

--- lib/logstash/codecs/cloudtrail.rb
+++ lib/logstash/codecs/cloudtrail.rb
@@ -11,6 +11,16 @@ class LogStash::Codecs::CloudTrail < LogStash::Codecs::Spool
   def decode(data)
     super(LogStash::Json.load(data.force_encoding("UTF-8"))['Records']) do |event|
       event['@timestamp'] = event.delete('eventTime')
+
+      if event.has_key?("requestParameters")
+        if event['requestParameters'].has_key?("disableApiTermination")
+          if event['requestParameters']['disableApiTermination'].class != Hash
+            disableApiTermination = event['requestParameters'].delete('disableApiTermination')
+            event['requestParameters']['disableApiTermination']= {"value" => disableApiTermination}
+          end
+        end
+      end
+
       yield LogStash::Event.new(event)
     end
   end # def decode
