ec7fb6f08746d021effc7483ce69bf21c3bdd5f6
Patrick Robinson <patrick.robinson@envato.com>
2016-12-03 05:52:22 +1100

    Update to logstash 5.0 (#11)
    * Refactor to no longer use super
    
    As per plugin guidlines
    https://www.elastic.co/guide/en/logstash/current/_how_to_write_a_logstash_codec_plugin.html
    
    * Remove spool and bump versions
    
    * Ensure we use jdk8

--- lib/logstash/codecs/cloudtrail.rb
+++ lib/logstash/codecs/cloudtrail.rb
@@ -1,15 +1,24 @@
 # encoding: utf-8
 require "logstash/codecs/base"
-require "logstash/codecs/spool"
 require "logstash/json"
+require "logstash/util/charset"
 
 # This is the base class for logstash codecs.
-class LogStash::Codecs::CloudTrail < LogStash::Codecs::Spool
+class LogStash::Codecs::CloudTrail < LogStash::Codecs::Base
   config_name "cloudtrail"
+
+  config :charset, :validate => ::Encoding.name_list, :default => "UTF-8"
+
+  public
+  def register
+    @converter = LogStash::Util::Charset.new(@charset)
+    @converter.logger = @logger
+  end
 
   public
   def decode(data)
-    super(LogStash::Json.load(data.force_encoding("UTF-8"))['Records']) do |event|
+    decoded = LogStash::Json.load(@converter.convert(data))
+    decoded['Records'].each do |event|
       event['@timestamp'] = event.delete('eventTime')
       yield LogStash::Event.new(event)
     end
