35c613fe103a077c595ffb65656a67eceb9b643c
Rob Bavey <rob.bavey@elastic.co>
2018-05-30 15:32:56 -0400

    Handle 'sourceIpAddress' fields with non-ip address content
    API calls run from AWS support will appear in AWS logs with
    a hostname value, rather than an IP address along the lines of
    "support.amazonaws.com". This commit addresses this issue by
    moving such fields to a separate 'sourceHost' field in the
    event
    
    Fixes #20

--- spec/codecs/cloudtrail_spec.rb
+++ spec/codecs/cloudtrail_spec.rb
@@ -16,5 +16,26 @@ describe LogStash::Codecs::CloudTrail do
         subject.decode('{"Records":[{"requestParameters":null}]}', &b)
       }.to yield_control
     end
+
+    it 'accepts records with a valid sourceIpAddress' do
+      subject.decode('{"Records":[{"sourceIpAddress":"111.22.3.3"}]}') do |event|
+       expect(event.get("sourceIpAddress")).to eq("111.22.3.3")
+       expect(event.get("sourceHost")).to be_nil
+      end
+    end
+
+    it 'accepts records with an invalid sourceIpAddress' do
+      subject.decode('{"Records":[{"sourceIpAddress":"www.elastic.co"}]}') do |event|
+        expect(event.get("sourceIpAddress")).to be_nil
+        expect(event.get("sourceHost")).to eq("www.elastic.co")
+      end
+    end
+
+    it 'accepts records with a no sourceIpAddress' do
+      subject.decode('{"Records":[{"sourceIpAddress":null}]}') do |event|
+        expect(event.get("sourceIpAddress")).to be_nil
+        expect(event.get("sourceHost")).to be_nil
+      end
+    end
   end
 end
